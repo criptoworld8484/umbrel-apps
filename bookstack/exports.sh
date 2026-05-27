a8d0e6a316aa36cf380ad7d21f86dfb01a0ba4c2
criptoworld <criptowordl8484@proton.me>
2026-05-27 14:44:02 +0200

    Keep only frigate-silent-payments app, remove all others

--- bookstack/exports.sh
+++
@@ -1,3 +0,0 @@
-export APP_BOOKSTACK_APP_KEY=$(derive_entropy "env-${app_entropy_identifier}-APP_KEY" | head -c32)
-export APP_BOOKSTACK_DB_PASSWORD=$(derive_entropy "env-${app_entropy_identifier}-DB_PASSWORD" | head -c32)
-export APP_BOOKSTACK_DB_ROOT_PASSWORD=$(derive_entropy "env-${app_entropy_identifier}-DB_ROOT_PASSWORD" | head -c32)
--- nostr-vpn/exports.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-# Nostr VPN does not export environment variables for dependent Umbrel apps.
